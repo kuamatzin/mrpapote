5ee2a97615aa13369d138bd5f3c50b68b6a0bd91
Carlos Cuamatzin <kuamatzin@gmail.com>
2017-08-28 16:27:41 -0500

    Testing 3

--- app/Http/Controllers/StatsController.php
+++ app/Http/Controllers/StatsController.php
@@ -7,6 +7,7 @@ use App\Product;
 use Carbon\Carbon;
 use ConsoleTVs\Charts\Facades\Charts;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Input;
 
 class StatsController extends Controller
@@ -14,7 +15,17 @@ class StatsController extends Controller
     public function orders()
     {
         $start_date = Input::get('start_date');
+        dd($start_date);
         $end_date = Input::get('end_date');
+
+        $values = [];
+        $months = [];
+
+        for ($start_date; $start_date <= $end_date; $start_date ++){
+            $months[] = Carbon::createFromFormat('!m', $start_date)->format('F');
+            array_push($values, Auth::user()->orders()->whereMonth('created_at', '=', $start_date)->count());
+        }
+
 
         $chart = Charts::create('bar', 'highcharts')
             // Setup the chart settings
@@ -22,17 +33,7 @@ class StatsController extends Controller
             // A dimension of 0 means it will take 100% of the space
             ->dimensions(0, 400) // Width x Height
             // This defines a preset of colors already done:)
-            ->template("material");
-
-        $values = [];
-        $months = [];
-        for ($start_date; $start_date <= $end_date; $start_date ++){
-            $months[] = Carbon::createFromFormat('!m', $start_date)->format('F');
-            array_push($values, Order::whereMonth('created_at', '=', $start_date)->count());
-        }
-
-
-        $chart
+            ->template("material")
             ->values($values)
             ->elementLabel("Ventas")
             // Setup what the values mean
