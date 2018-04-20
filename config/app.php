abcb685ccebc71741b4c7ba27ab11866c4d1ad1a
Carlos Cuamatzin <kuamatzin@gmail.com>
2018-04-20 10:24:52 -0500

    Delete Chart ConsoleTV

--- config/app.php
+++ config/app.php
@@ -228,7 +228,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Charts' => ConsoleTVs\Charts\Facades\Charts::class,
         'Socialite' => Laravel\Socialite\Facades\Socialite::class,
         'Excel' => Maatwebsite\Excel\Facades\Excel::class,
     ],
